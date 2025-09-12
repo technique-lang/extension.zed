b287afcfd50776eaf939687351da9977eebca474
Andrew Cowie <istathar@gmail.com>
2025-09-12 12:21:22 +1000

    Initial struct definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
-pub const GREETING : &'static str = "Hello";
+use zed_extension_api as zed;
+
+struct TechniqueExtension {
+    cached_binary_path: Option<String>,
+}
+
+impl zed::Extension for TechniqueExtension {
+    fn new() -> Self {
+        Self {
+            cached_binary_path: None,
+        }
+    }
+}
+
+zed::register_extension!(TechniqueExtension);
