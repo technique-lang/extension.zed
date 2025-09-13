192331572720cbcd5ee44a0b274e15a6b29ee10f
Andrew Cowie <istathar@gmail.com>
2025-09-13 14:59:06 +1000

    Hard-code binary location

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,21 @@ impl zed::Extension for TechniqueExtension {
             cached_binary_path: None,
         }
     }
+
+    fn language_server_command(
+        &mut self,
+        _language_server_id: &zed::LanguageServerId,
+        _worktree: &zed::Worktree,
+    ) -> Result<zed::Command, String> {        
+        let path = "/home/andrew/src/technique-lang/technique/target/debug/technique";
+        self.cached_binary_path = Some(path.to_string());
+
+        let command = path.to_string();
+        let args = vec!["language".to_string()];
+        let env = vec![];
+
+        Ok(zed::Command { command, args, env })
+    }
 }
 
 zed::register_extension!(TechniqueExtension);
