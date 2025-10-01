8a65852e159acd60e4e54e58ed2b51e5913f8017
Andrew Cowie <istathar@gmail.com>
2025-10-01 12:24:26 +1000

    Show procedure names and titles in outline

---
+++ languages/technique/outline.scm
@@ -0,0 +1,14 @@
+(procedure
+  (declaration
+    (procedure_name) @name
+    (declaration_marker) @context.extra
+    (signature)? @context.extra
+  )
+) @item
+
+(procedure
+  (title
+    (title_marker)
+    (title_text) @name 
+  )
+) @item
